3adc48352d17749540cc9f3e775cffaaf521bc80
jhs512 <jangka512@gmail.com>
2024-04-08 21:59:52 +0900

    work

--- src/main/java/com/ll/demo03/HomeController.java
+++ src/main/java/com/ll/demo03/HomeController.java
@@ -1,14 +1,18 @@
 package com.ll.demo03;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @Value("${custom.site.name}")
+    private String siteName;
+
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Hello, World!";
+        return "Hello, World!, on " + siteName;
     }
 }
