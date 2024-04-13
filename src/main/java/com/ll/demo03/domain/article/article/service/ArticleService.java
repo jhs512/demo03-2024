485ce6c2de13a7ef7b70a236f371d7c69397f96e
jhs512 <jangka512@gmail.com>
2024-04-13 11:11:20 +0900

    work

--- src/main/java/com/ll/demo03/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/demo03/domain/article/article/service/ArticleService.java
@@ -2,6 +2,7 @@ package com.ll.demo03.domain.article.article.service;
 
 import com.ll.demo03.domain.article.article.entity.Article;
 import com.ll.demo03.domain.article.article.repository.ArticleRepository;
+import com.ll.demo03.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +18,16 @@ public class ArticleService {
         return articleRepository.count();
     }
 
-    public Article write(String title, String body) {
+    public RsData<Article> write(String title, String body) {
         Article article = Article
                 .builder()
                 .title(title)
                 .body(body)
                 .build();
 
-        return articleRepository.save(article);
+        articleRepository.save(article);
+
+        return RsData.of("%d번 게시물이 작성되었습니다.".formatted(article.getId()), article);
     }
 
     public void delete(Article article) {
