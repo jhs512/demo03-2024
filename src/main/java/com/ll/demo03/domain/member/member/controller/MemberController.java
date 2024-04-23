689e6931fb8e635f6a98ecfb46056bc594102a1f
jhs512 <jangka512@gmail.com>
2024-04-23 11:16:43 +0900

    work

--- src/main/java/com/ll/demo03/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/demo03/domain/member/member/controller/MemberController.java
@@ -2,8 +2,8 @@ package com.ll.demo03.domain.member.member.controller;
 
 import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.service.MemberService;
-import com.ll.demo03.global.exceptions.GlobalException;
 import com.ll.demo03.global.rsData.RsData;
+import com.ll.demo03.standard.dto.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,16 +19,16 @@ public class MemberController {
     public RsData join(
             String username, String password, String nickname
     ) {
-        if (username == null || username.isBlank()) {
-            throw new GlobalException("400-1", "아이디를 입력해주세요.");
+        if (Ut.str.isBlank(username)) {
+            return RsData.of("400-1", "아이디를 입력해주세요.");
         }
 
-        if (password == null || password.isBlank()) {
-            throw new GlobalException("400-2", "비밀번호를 입력해주세요.");
+        if (Ut.str.isBlank(password)) {
+            return RsData.of("400-2", "비밀번호를 입력해주세요.");
         }
 
-        if (nickname == null || nickname.isBlank()) {
-            throw new GlobalException("400-3", "닉네임을 입력해주세요.");
+        if (Ut.str.isBlank(nickname)) {
+            return RsData.of("400-3", "닉네임을 입력해주세요.");
         }
 
         RsData<Member> joinRs = memberService.join(username, password, nickname);
