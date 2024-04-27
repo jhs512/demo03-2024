4132f2850b013be3ff7ad6f3a12cfac044aed8c1
jhs512 <jangka512@gmail.com>
2024-04-27 17:40:33 +0900

    work

--- src/main/java/com/ll/demo03/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/demo03/domain/member/member/controller/ApiV1MemberController.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Slf4j
 public class ApiV1MemberController {
     private final MemberService memberService;
@@ -40,6 +42,7 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("")
+    @Transactional
     public RsData<MemberJoinRespBody> join(
             @RequestBody @Valid MemberJoinReqBody reqBody
     ) {
