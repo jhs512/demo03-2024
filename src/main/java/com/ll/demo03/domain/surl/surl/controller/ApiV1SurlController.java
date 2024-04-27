fca7e59f41785a21053632b2dc6d1895b328a610
jhs512 <jangka512@gmail.com>
2024-04-27 16:05:29 +0900

    work

---
+++ src/main/java/com/ll/demo03/domain/surl/surl/controller/ApiV1SurlController.java
@@ -0,0 +1,54 @@
+package com.ll.demo03.domain.surl.surl.controller;
+
+import com.ll.demo03.domain.member.member.entity.Member;
+import com.ll.demo03.domain.surl.surl.entity.Surl;
+import com.ll.demo03.domain.surl.surl.service.SurlService;
+import com.ll.demo03.global.rq.Rq;
+import com.ll.demo03.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/surls")
+@RequiredArgsConstructor
+@Slf4j
+public class ApiV1SurlController {
+    private final SurlService surlService;
+    private final Rq rq;
+
+    @AllArgsConstructor
+    @Getter
+    public static class SurlAddReqBody {
+        @NotBlank
+        private String body;
+        @NotBlank
+        private String url;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class SurlAddRespBody {
+        private Surl item;
+    }
+
+    @PostMapping("")
+    public RsData<SurlAddRespBody> add(
+            @RequestBody @Valid SurlAddReqBody reqBody
+    ) {
+        Member member = rq.getMember(); // 현재 브라우저로 로그인한 회원
+
+        RsData<Surl> addRs = surlService.add(member, reqBody.body, reqBody.url);
+
+        return addRs.newDataOf(
+                new SurlAddRespBody(addRs.getData())
+        );
+    }
+}
