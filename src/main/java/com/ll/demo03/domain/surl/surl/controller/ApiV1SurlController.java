d835db53c0ed9949c950423fc709042fcee09307
jhs512 <jangka512@gmail.com>
2024-04-28 15:01:26 +0900

    work

--- src/main/java/com/ll/demo03/domain/surl/surl/controller/ApiV1SurlController.java
+++ src/main/java/com/ll/demo03/domain/surl/surl/controller/ApiV1SurlController.java
@@ -2,6 +2,7 @@ package com.ll.demo03.domain.surl.surl.controller;
 
 import com.ll.demo03.domain.auth.auth.service.AuthService;
 import com.ll.demo03.domain.member.member.entity.Member;
+import com.ll.demo03.domain.member.member.service.MemberService;
 import com.ll.demo03.domain.surl.surl.dto.SurlDto;
 import com.ll.demo03.domain.surl.surl.entity.Surl;
 import com.ll.demo03.domain.surl.surl.service.SurlService;
@@ -28,6 +29,7 @@ import java.util.List;
 public class ApiV1SurlController {
     private final SurlService surlService;
     private final AuthService authService;
+    private final MemberService memberService;
     private final Rq rq;
 
 
@@ -92,7 +94,12 @@ public class ApiV1SurlController {
     }
 
     @GetMapping("")
-    public RsData<SurlGetItemsRespBody> getItems() {
+    public RsData<SurlGetItemsRespBody> getItems(
+            String actorUsername
+    ) {
+        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(GlobalException.E404::new);
+        rq.setMember(loginedMember);
+
         Member member = rq.getMember();
 
         List<Surl> surls = surlService.findByAuthorOrderByIdDesc(member);
