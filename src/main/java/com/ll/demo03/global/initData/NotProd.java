22da61a9ae5b92682aa9cfab4352dd42a87b2bbc
jhs512 <jangka512@gmail.com>
2024-04-29 11:08:21 +0900

    work

--- src/main/java/com/ll/demo03/global/initData/NotProd.java
+++ src/main/java/com/ll/demo03/global/initData/NotProd.java
@@ -4,6 +4,8 @@ import com.ll.demo03.domain.article.article.entity.Article;
 import com.ll.demo03.domain.article.article.service.ArticleService;
 import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.service.MemberService;
+import com.ll.demo03.domain.surl.surl.entity.Surl;
+import com.ll.demo03.domain.surl.surl.service.SurlService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -24,6 +26,7 @@ public class NotProd {
     private NotProd self;
     private final MemberService memberService;
     private final ArticleService articleService;
+    private final SurlService surlService;
 
     @Bean
     @Order(4)
@@ -45,5 +48,11 @@ public class NotProd {
 
         Article article3 = articleService.write(memberUser2, "제목 3", "내용 3").getData();
         Article article4 = articleService.write(memberUser2, "제목 4", "내용 4").getData();
+
+        Surl surl1 = surlService.add(memberUser1, "네이버", "https://www.naver.com").getData();
+        Surl surl2 = surlService.add(memberUser1, "다음", "https://www.daum.net").getData();
+
+        Surl surl3 = surlService.add(memberUser2, "구글", "https://www.google.com").getData();
+        Surl surl4 = surlService.add(memberUser2, "네이버", "https://www.naver.com").getData();
     }
 }
