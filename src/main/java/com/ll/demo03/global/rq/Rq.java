cd577c5496a13ee5547c15e991f28389dc17cd2b
jhs512 <jangka512@gmail.com>
2024-04-29 19:30:45 +0900

    work

--- src/main/java/com/ll/demo03/global/rq/Rq.java
+++ src/main/java/com/ll/demo03/global/rq/Rq.java
@@ -17,16 +17,24 @@ public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
     private final MemberService memberService;
+    private Member member;
 
     public Member getMember() {
+        if (member != null) return member;
+
         String actorUsername = req.getParameter("actorUsername");
         String actorPassword = req.getParameter("actorPassword");
+
+        if ( actorUsername == null ) actorUsername = req.getHeader("actorUsername");
+        if ( actorPassword == null ) actorPassword = req.getHeader("actorPassword");
 
         if (Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디)를 입력해주세요.");
         if (Ut.str.isBlank(actorPassword)) throw new GlobalException("401-2", "인증정보(비밀번호)를 입력해주세요.");
 
         Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("403-3", "해당 회원이 존재하지 않습니다."));
         if (!loginedMember.getPassword().equals(actorPassword)) throw new GlobalException("403-4", "비밀번호가 일치하지 않습니다.");
+
+        member = loginedMember;
 
         return loginedMember;
     }
