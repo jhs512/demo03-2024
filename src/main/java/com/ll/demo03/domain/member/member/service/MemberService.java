102a021bc8c1f5f82553f4ac8d604207d26fcc8c
jhs512 <jangka512@gmail.com>
2024-05-01 00:10:02 +0900

    work

--- src/main/java/com/ll/demo03/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/demo03/domain/member/member/service/MemberService.java
@@ -5,6 +5,7 @@ import com.ll.demo03.domain.member.member.repository.MemberRepository;
 import com.ll.demo03.global.exceptions.GlobalException;
 import com.ll.demo03.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
@@ -28,7 +30,7 @@ public class MemberService {
 
         Member member = Member.builder()
                 .username(username)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .nickname(nickname)
                 .build();
 
