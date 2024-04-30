24663d9ce6c5dcf3b9c7c3062de34994791feb0d
jhs512 <jangka512@gmail.com>
2024-05-01 00:28:36 +0900

    work

--- src/main/java/com/ll/demo03/global/security/SecurityConfig.java
+++ src/main/java/com/ll/demo03/global/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.ll.demo03.global.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -34,7 +41,8 @@ public class SecurityConfig {
                         formLogin ->
                                 formLogin
                                         .permitAll()
-                );
+                )
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
